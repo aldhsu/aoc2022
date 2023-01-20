1f1f2a448e06a7cdc57a3aad1eccca8fb5d9c690
Allen Hsu <allen.hsusp+git@gmail.com>
2023-01-20 22:59:35 +1000

    Complete day18 part2.

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -4,6 +4,8 @@ fn main() {
     let input = include_str!("../input.txt");
     let part1 = part1(input);
     println!("part1: {part1}");
+    let part2 = part2(input);
+    println!("part2: {part2}");
 }
 
 type Coord = (isize, isize);
@@ -51,8 +53,6 @@ fn part1(s: &str) -> usize {
                     let Some(neighbour) = neighbour_coord(coord, offset) else { continue };
                     if !coords.contains(&neighbour) {
                         total += 1;
-                    } else {
-                        dbg!(neighbour);
                     }
                 }
             }
@@ -86,4 +86,103 @@ fn part1_works() {
     let input = r#"0,0,0
 1,0,0"#;
     assert_eq!(part1(input), 10);
+}
+
+#[test]
+fn part2_works() {
+    let input = r#"2,2,2
+1,2,2
+3,2,2
+2,1,2
+2,3,2
+2,2,1
+2,2,3
+2,2,4
+2,2,6
+1,2,5
+3,2,5
+2,1,5
+2,3,5"#;
+    assert_eq!(part2(input), 58);
+}
+
+type Vxt = (isize, isize, isize);
+
+const VXT_OFFSETS: [Vxt; 6] = [
+    (1, 0, 0),
+    (-1, 0, 0),
+    (0, 1, 0),
+    (0, -1, 0),
+    (0, 0, 1),
+    (0, 0, -1),
+];
+
+fn part2(s: &str) -> usize {
+    let mut collection: BTreeSet<Vxt> = BTreeSet::new();
+
+    let mut max_x = 0;
+    let mut max_y = 0;
+    let mut max_z = 0;
+
+    let mut min_x = 0;
+    let mut min_y = 0;
+    let mut min_z = 0;
+
+    for line in s.lines() {
+        let mut iter = line.splitn(3, ',');
+        let x = iter
+            .next()
+            .expect("couldn't get x")
+            .parse::<isize>()
+            .expect("couldn't parse x");
+        let y = iter
+            .next()
+            .expect("couldn't get y")
+            .parse::<isize>()
+            .expect("couldn't parse y");
+        let z = iter
+            .next()
+            .expect("couldn't get z")
+            .parse::<isize>()
+            .expect("couldn't parse z");
+        collection.insert((x, y, z));
+
+        max_x = max_x.max(x);
+        max_y = max_y.max(y);
+        max_z = max_z.max(z);
+
+        min_x = min_x.min(x);
+        min_y = min_y.min(y);
+        min_z = min_z.min(z);
+    }
+
+    let mut total = 0;
+    let mut seen = BTreeSet::new();
+    let mut work = vec![(min_x - 1, min_y - 1, min_z - 1)];
+
+    while let Some(water) = work.pop() {
+        if !seen.insert(water) {
+            continue;
+        }
+
+        if water.0 < min_x - 1 || water.0 > max_x + 1 {
+            continue;
+        }
+        if water.1 < min_y - 1 || water.1 > max_y + 1 {
+            continue;
+        }
+        if water.2 < min_z - 1 || water.2 > max_z + 1 {
+            continue;
+        }
+
+        for (ox, oy, oz) in VXT_OFFSETS {
+            let neighbour = (water.0 + ox, water.1 + oy, water.2 + oz);
+            if collection.contains(&neighbour) {
+                total += 1;
+            } else {
+                work.push(neighbour);
+            }
+        }
+    }
+    total
 }
